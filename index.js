461add3f1f6ab608efaf58ddce8a216956ddc68e
Chris Tate <chris@studiotate.com>
2015-04-13 13:18:38 -0500

    Add ability to combine arrays

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ var hasOwn = Object.prototype.hasOwnProperty;
 var toString = Object.prototype.toString;
 var undefined;
 
+var combineArrays = function combineArrays() {
+  'use strict';
+  var combined = [];
+  for (var i = 0; i < arguments.length; i++) {
+		for (var n = 0; n < arguments[i].length; n++) {
+			if (combined.indexOf(arguments[i][n]) === -1) {
+				combined.push(arguments[i][n]);
+			}
+		}
+  }
+  combined.sort();
+  return combined;
+};
+
+var isPlainArray = function isPlainArray() {
+	'use strict';
+	if (!arguments || !arguments.length) {
+		return false;
+	}
+	for (var i = 0; i < arguments.length; i++) {
+		if (!Array.isArray(arguments[i])) {
+			return false;
+		}
+		for (var n = 0; n < arguments[i].length; n++) {
+			if (typeof arguments[i][n] != 'string' && typeof arguments[i][n] != 'number') {
+				return false;
+			}
+		}
+	}
+	
+	return true;
+};
+
 var isPlainObject = function isPlainObject(obj) {
 	'use strict';
 	if (!obj || toString.call(obj) !== '[object Object]') {
@@ -29,7 +62,8 @@ module.exports = function extend() {
 		target = arguments[0],
 		i = 1,
 		length = arguments.length,
-		deep = false;
+		deep = false,
+		combine = false;
 
 	// Handle a deep copy situation
 	if (typeof target === 'boolean') {
@@ -37,14 +71,26 @@ module.exports = function extend() {
 		target = arguments[1] || {};
 		// skip the boolean and the target
 		i = 2;
+		if (typeof arguments[1] === 'boolean') {
+			combine = arguments[1];
+			target = arguments[2] || {};
+			i = 3;
+		}
 	} else if ((typeof target !== 'object' && typeof target !== 'function') || target == null) {
 		target = {};
 	}
 
 	for (; i < length; ++i) {
 		options = arguments[i];
+		
 		// Only deal with non-null/undefined values
 		if (options != null) {
+			// Combine if we're combining plain arrays
+			if (combine && isPlainArray(options, target)) {
+				target = combineArrays(target, options);
+				continue;
+			}
+			
 			// Extend the base object
 			for (name in options) {
 				src = target[name];
@@ -65,7 +111,11 @@ module.exports = function extend() {
 					}
 
 					// Never move original objects, clone them
-					target[name] = extend(deep, clone, copy);
+					if (combine) {
+						target[name] = extend(deep, combine, clone, copy);
+					} else {
+						target[name] = extend(deep, clone, copy);
+					}
 
 				// Don't bring in undefined values
 				} else if (copy !== undefined) {
